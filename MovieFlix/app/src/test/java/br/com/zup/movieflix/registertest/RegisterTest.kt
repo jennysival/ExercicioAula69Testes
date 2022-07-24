24c505b582f9eb1837c1d46253dfeb4d9f9cfb4a
jennysival <jeniffersival@gmail.com>
2022-07-24 19:16:45 -0300

    Implementar teste unitário para verificação de usuário registrado

--- MovieFlix/app/src/test/java/br/com/zup/movieflix/registertest/RegisterTest.kt
+++ MovieFlix/app/src/test/java/br/com/zup/movieflix/registertest/RegisterTest.kt
@@ -1,4 +1,21 @@
 package br.com.zup.movieflix.registertest
 
+import br.com.zup.movieflix.register.model.RegisterModel
+import br.com.zup.movieflix.register.repositorio.RegisterRepository
+import org.junit.Test
+import org.junit.Assert.*
+
 class RegisterTest {
+
+    @Test
+    fun registerUser(){
+        val user = RegisterRepository().registerUser(RegisterModel(
+            userName = "Felipe",
+            email = "felipe.dearaujo@zup.com.br",
+            password = "melhorProfessorDoMundo"))
+
+        assertEquals(user.userName, "Felipe")
+        assertEquals(user.email, "felipe.dearaujo@zup.com.br")
+        assertEquals(user.password, "melhorProfessorDoMundo")
+    }
 }
